848c6ca37afb51ec216da7c9c6d4b9eca9feec45
Larissv <barroslarissav@gmail.com>
2022-01-17 14:45:20 -0300

    Android

--- app/src/main/java/com/cursoandroid/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/cursoandroid/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,9 +2,13 @@ package com.cursoandroid.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -50,5 +54,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout
                 .simple_list_item_1, dao.todos()));
+        listaDeAlunos.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Log.i("posicao aluno", "onItemClick: ");
+            });
+        }
     }
 }
